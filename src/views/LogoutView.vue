7b125fc55ed7e1408c5df61d3da905db10f6d6fd
Artyom Chernetsov <chernetsov.artyom@gmail.com>
2023-05-08 00:52:30 +0300

    Logout

--- src/views/LogoutView.vue
+++ src/views/LogoutView.vue
@@ -6,9 +6,9 @@ import PageContentLayout from '@/containers/PageContentLayout.vue'
     <PageContentLayout>
         <div class="card bg-base-100 shadow-xl">
             <div class="card-body space-y-2">
-                <h2 class="card-title">Confirm logout</h2>
+                <h2 class="card-title">Logout?</h2>
                 <div class="card-actions justify-center">
-                    <button class="btn btn-primary">Logout</button>
+                    <button class="btn btn-primary">Yes</button>
                 </div>
             </div>
         </div>
