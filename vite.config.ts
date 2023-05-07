c4580e6cdf60c2f328aa5ea7c04c6bf7220320b9
Artyom Chernetsov <chernetsov.artyom@gmail.com>
2023-05-07 20:04:55 +0300

    Clean template things

--- vite.config.ts
+++ vite.config.ts
@@ -6,14 +6,10 @@ import { defineConfig } from 'vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   // fixes for gitabl pages: https://forum.gitlab.com/t/no-access-control-allow-origin-header-for-https-projects-gitlab-io-auth/54904
-  base: '/daisyui-vue-admin-minimal',
   plugins: [vue()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
-  build: {
-    outDir: 'public'
-  }
 })
