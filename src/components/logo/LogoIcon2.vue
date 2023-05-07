99ae8c57b9d5325a2eb87cd485f02015b967bc00
Artyom Chernetsov <chernetsov.artyom@gmail.com>
2023-05-08 00:10:08 +0300

    Branding

---
+++ src/components/logo/LogoIcon2.vue
@@ -0,0 +1,23 @@
+<!-- https://www.svgrepo.com/svg/191084/parrot -->
+<template>
+    <svg height="40px" width="40px" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg"
+        xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 512 512" xml:space="preserve" fill="#000000"
+        transform="matrix(-1, 0, 0, 1, 0, 0)">
+    <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+    <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
+    <g id="SVGRepo_iconCarrier">
+        <path style="fill:#26a269;"
+            d="M83.027,186.811v27.676c0,68.782,55.758,124.541,124.541,124.541h27.676V186.811H83.027z"></path>
+        <path style="fill:#8ff0a4;" d="M207.568,6.919C92.931,6.919,0,99.85,0,214.486h235.243V6.919H207.568z"></path>
+        <path style="fill:#2ec27e;"
+            d="M207.568,6.919v332.108c0,91.709,74.345,166.054,166.054,166.054H512V6.919H207.568z"></path>
+        <path style="fill:#26a269;" d="M359.784,6.919v497.584c4.564,0.376,9.177,0.578,13.838,0.578H512V6.919H359.784z">
+        </path>
+        <circle style="fill:#F8F8F9;" cx="359.784" cy="159.135" r="83.027"></circle>
+        <path style="fill:#EDEDED;"
+            d="M359.784,76.108v166.054c45.854,0,83.027-37.173,83.027-83.027S405.638,76.108,359.784,76.108z"></path>
+        <path style="fill:#58575D;"
+            d="M359.784,186.811h-0.003c-15.259,0-27.673-12.415-27.673-27.676 c0.006-15.263,12.421-27.676,27.676-27.676c15.26,0,27.676,12.415,27.676,27.676S375.044,186.811,359.784,186.811z">
+        </path>
+    </g>
+</svg></template>
---
+++ src/components/logo/LogoIcon4.vue
@@ -0,0 +1,35 @@
+<!-- https://www.svgrepo.com/svg/452920/parrot -->
+<template>
+    <svg width="40px" height="40px" viewBox="0 0 400 400" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+        <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
+        <g id="SVGRepo_iconCarrier">
+            <path
+                d="M191.236 117.086C194.501 96.0678 211.099 44.1669 252.916 72.0365C257.607 75.163 247.848 116.259 249.371 124.101C261.162 184.809 261.162 228.712 216.484 261.748"
+                stroke="#000000" stroke-opacity="0.9" stroke-width="16" stroke-linecap="round" stroke-linejoin="round">
+            </path>
+            <path
+                d="M258.489 72.1705C309.397 67.8918 292.64 128.684 282.823 135.598C282.77 135.635 285.957 128.684 279.905 121.239C276.179 116.656 261.575 116.656 253.742 118.827"
+                stroke="#000000" stroke-opacity="0.9" stroke-width="16" stroke-linecap="round" stroke-linejoin="round">
+            </path>
+            <path
+                d="M153.732 218.421C155.675 204.396 153.266 189.23 156.236 175.143C161.024 152.403 175.99 102.951 206.912 134.408C247.866 176.071 202.624 247.334 163.116 267.43"
+                stroke="#000000" stroke-opacity="0.9" stroke-width="16" stroke-linecap="round" stroke-linejoin="round">
+            </path>
+            <path
+                d="M187.827 281.068C187.301 296.043 188.77 319.579 174.941 329.477C164.961 336.619 172.119 274.513 167.852 292.823C165.455 303.121 124.497 385.02 134.992 294.9"
+                stroke="#000000" stroke-opacity="0.9" stroke-width="16" stroke-linecap="round" stroke-linejoin="round">
+            </path>
+            <path d="M210.269 268.567C210.572 270.823 209.846 273.116 209.635 275.386" stroke="#000000" stroke-opacity="0.9"
+                stroke-width="16" stroke-linecap="round" stroke-linejoin="round"></path>
+            <path d="M223.941 275.321C223.73 275.744 223.519 276.163 223.311 276.586" stroke="#000000" stroke-opacity="0.9"
+                stroke-width="16" stroke-linecap="round" stroke-linejoin="round"></path>
+            <path d="M234.078 268.567C234.668 270.659 233.659 272.357 233.447 274.25" stroke="#000000" stroke-opacity="0.9"
+                stroke-width="16" stroke-linecap="round" stroke-linejoin="round"></path>
+            <path d="M106 271.32C168.389 275.643 230.674 257.202 292.383 257.202" stroke="#000000" stroke-opacity="0.9"
+                stroke-width="16" stroke-linecap="round" stroke-linejoin="round"></path>
+            <path d="M233.854 93.5486C233.854 91.6534 233.854 89.7615 233.854 87.8662" stroke="#000000" stroke-opacity="0.9"
+                stroke-width="16" stroke-linecap="round" stroke-linejoin="round"></path>
+        </g>
+    </svg>
+</template>
